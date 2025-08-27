f4fe2587b1bf9111fa3e3b4c9ac9dc80144fbf7b
Bruno Guilherme <brunogui29@outlook.com>
2025-08-27 18:55:17 -0300

    feat:mudança título sobre

--- src/components/Sobre/Sobre.js
+++ src/components/Sobre/Sobre.js
@@ -4,7 +4,7 @@ function Sobre() {
     return(
         <div className='sobre-container'>
             
-            <h1 className='sobre-texto' id='sobre'>Sobre</h1>
+            <h1 className='sobre-texto' id='sobre'>Viva este momento!</h1>
             <p className='sobre-texto' id='sobre-descricao'>Este é um portal criado com o intuito de ajudar pessoas a encontrarem o espaço de eventos adequado para sua festa, também ajudando anfitriões a alugarem seus imóveis.</p>
             
         </div>
